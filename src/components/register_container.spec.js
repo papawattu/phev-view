8771b4119a831b790f965ff5d4042abe7510ff30
Jamie Nuttall <jamie@wattu.com>
2017-07-18 10:50:36 +0100

    fix for jsdom ci issue

--- src/components/register_container.spec.js
+++ src/components/register_container.spec.js
@@ -2,20 +2,20 @@ import chai from 'chai';
 import sinon from 'sinon';
 import RegisterContainer from './register_container';
 import {Map,List,fromJS} from 'immutable';
-import 'jsdom-global/register';
 import pretty from 'pretty';
+import { JSDOM } from 'jsdom'
 
 const assert = chai.assert;
 
 const html = '<table><thead><tr><th>Register</th><th>Data</th></tr></thead><tbody><tr><td>1</td><td>0</td></tr><tr><td>2</td><td>0</td><td>1</td><td>2</td><td>3</td></tr></tbody></table>'
 
+const { document } = new JSDOM().window;
 
 const registers = {
     1: [0],
     2: [0,1,2,3]
 };
 const map = fromJS(registers);
-
 
 describe('Register Container', () => {
 
@@ -25,7 +25,7 @@ describe('Register Container', () => {
     });
     it('Should return a table', () => {
         const sut = RegisterContainer( { document, registers: map } );
-        assert.equal(pretty(sut.outerHTML), pretty(html));
+        assert.equal(sut.outerHTML, html);
     });
 
 });
