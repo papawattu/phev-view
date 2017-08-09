69ab3173611e3e4f651a672cbb2bc0553f8c0f87
Jamie Nuttall <jamie@wattu.com>
2017-08-09 20:37:55 +0100

    added battery attribute

--- src/components/summary.js
+++ src/components/summary.js
@@ -6,10 +6,13 @@ function summary({ labels, registers }) {
         return data;
     }, {});
     const toString = (arr, s, e) => String.fromCharCode(...arr.slice(s, e));
+    const toDate = x => x;
     return {
         vin: toString(regs.KO_WF_VIN_INFO_EVR, 1, 18),
         ssid: toString(regs.KO_WF_SSID_EVR, 0, regs.KO_WF_SSID_EVR.findIndex(x => x === 0xff)),
         ecuVersion: toString(regs.KO_ECU_VERSION_EVR, 0, regs.KO_ECU_VERSION_EVR.findIndex(x => x === 0x00)),
+        batteryLevel: regs.KO_WF_BATT_LEVEL_INFO_REP_EVR[0],
+        lastUpdated: toDate(regs.KO_WF_DATE_INFO_SYNC_EVR),
     }
 };
 
