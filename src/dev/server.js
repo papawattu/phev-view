bcd7d9c6d8409d98a7d8ea27b34a850ab6d958cf
Jamie Nuttall <jamie@wattu.com>
2017-10-11 11:56:27 +0100

    added stub server

---
+++ src/dev/server.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const cors = require('cors')
+const app = express()
+
+app.use(cors())
+app.use(express.json())
+
+
+app.post('/send', (req, res) => {
+
+    console.log(`=> ${JSON.stringify(req.body)}`)
+
+    res.sendStatus(200)
+
+    console.log(`<= 200`)
+
+})
+
+app.listen(8080)
+console.log('Running stub server on port 8080')
--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 const App = require('./app').default;
 
-const config = process.env.PRODUCTION ? require('./config').default : require('./dev/config')
+const config = process.env.PRODUCTION ? require('./config').default : require('./dev/config').default
 
 const firebase = process.env.PRODUCTION ? require('firebase') : require('./dev/firebase-stub').default
 
